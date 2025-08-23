30775adf6a3bc68e3849383f29460b681c48bfff
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2025-08-23 02:24:01 +0200

    started to work on how to design the thing

--- calc/src/lib.rs
+++ calc/src/lib.rs
@@ -1,8 +1,11 @@
 use wasm_bindgen::prelude::*;
 
-use crate::web_functions::{alert, log, log_many, log_u32};
+use crate::{types::pokemon::Pokemon, web_functions::{alert, log, log_many, log_u32}};
 
 mod web_functions;
+mod types{
+    pub mod pokemon;
+}
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
@@ -10,4 +13,14 @@ pub fn greet(name: &str) {
     log_u32(42);
     log_many("Logging", "many values!");
     alert(&format!("Hello, {}!", name));
+}
+
+
+fn has_defender_more_hp(attacker: Pokemon, defender: Pokemon) -> bool{
+    attacker.hp < defender.hp
+}
+
+#[wasm_bindgen]
+pub fn calc_gen_redux(attacker: Pokemon, defender: Pokemon){
+    has_defender_more_hp(attacker, defender);
 }
---
+++ calc/src/types/pokemon.rs
@@ -0,0 +1,7 @@
+use wasm_bindgen::{prelude::*};
+
+#[wasm_bindgen]
+pub struct Pokemon{
+    pub hp: u16,
+    pub hp_max: u16,
+}
