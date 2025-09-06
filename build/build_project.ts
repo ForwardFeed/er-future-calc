36c435a3fdb82f31f7c9a7b2d0fb1d7f17547c3c
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2025-09-06 13:59:33 +0200

    small fix on the build process

--- build/build_project.ts
+++ build/build_project.ts
@@ -1,15 +1,16 @@
 import { exit } from "process";
 import { parseArgs } from "util";
+import type { ProjectConfigurationFile } from "./types/project_configuration";
 
 console.log('Starting the process to build the project')
 
 const {values: cli_arguments}  = parseArgs({
     args: Bun.argv,
     options: {
-        file_version: {
+        file_configuration: {
             type: "string",
             short: "f",
-            default: '../versions.json',
+            default: './project_configuration.json',
             required: false
         },
         selected_version: {
@@ -22,8 +23,8 @@ const {values: cli_arguments}  = parseArgs({
     strict: true,
     allowPositionals: true,
 })
-const file_versions = await Bun.file(cli_arguments.file_version).json()
+const file_configuration = await Bun.file(cli_arguments.file_configuration).json() as ProjectConfigurationFile
 if (!cli_arguments.selected_version){
-    console.error('no versions were selected, available versions: ', file_versions.versions.map((x: { name: any; }) => x.name).join(', '))
+    console.error('no versions were selected, available versions: ', file_configuration.versions.map(x => x.name).join(', '))
     exit(1)
 }
