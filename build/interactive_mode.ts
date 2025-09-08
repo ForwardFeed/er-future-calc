84a1626c3080da5c657e1dcc202807ca8f2e4702
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2025-09-08 02:30:32 +0200

    made an utility that allows me to recompile the calc while still having the UI on the side and launched, with some chaotic debugging

--- build/interactive_mode.ts
+++ build/interactive_mode.ts
@@ -1,18 +1,22 @@
-import { compile_calculator } from "./subprocesses"
+import { compile_calculator, run_ui_dev } from "./subprocesses"
 
-
-function cleanup_before_exit(){
-    console.log(`Cleaning up`)
-
-    process.exit()
-}
 
 export async function setup_interactive_mode(){
+
+    const proc_ui = await run_ui_dev()
+    
+    const cleanup_before_exit = async ()=>{
+        console.log(`Cleaning up`)
+        proc_ui.send("SIGINT")
+    }
+
     // clean up the process launched if:
     process.on('SIGINT', cleanup_before_exit) // Ctrl + C
     process.on('SIGTERM', cleanup_before_exit) // Terminate Program normally
     process.on('SIGKILL', cleanup_before_exit) // Force kill
     process.on('SIGHUP', cleanup_before_exit) // Terminal Connection Broken
+
+    
 
     const prompt = "Press Enter to recompile the calculator: ";
     process.stdout.write(prompt);
@@ -20,12 +24,9 @@ export async function setup_interactive_mode(){
     for await (const _line of console) {
         const proc_calculator_build = (await compile_calculator())
         if (await proc_calculator_build.exited){
-            console.error(`compilation returned non-zero code: ${await proc_calculator_build.stdout.text()}, with out`)
+            console.error(`compilation returned non-zero code: ${proc_calculator_build.exitCode}`)
             process.exit(11)
-        } else {
-            console.log(await proc_calculator_build.stdout.text())
         }
-
         process.stdout.write(prompt);
     }
     await Bun.sleep(1000);
--- build/subprocesses.ts
+++ build/subprocesses.ts
@@ -3,5 +3,26 @@ const BUILD_CALC_COMMAND = "bun run build-calc".split(' ')
 
 export async function compile_calculator(){
     const proc = Bun.spawn(BUILD_CALC_COMMAND)
+    setTimeout(async ()=>{
+        const decoder = new TextDecoder()
+        for await (const chunk of proc.stdout) {
+            console.log(`>CALC-BUILD: ${decoder.decode(chunk)}`)
+        }
+    })
+    return proc
+}
+
+const RUN_UI_DEV = "bun run run-ui".split(' ')
+
+export async function run_ui_dev(){
+    const proc = Bun.spawn(RUN_UI_DEV)
+    setTimeout(async ()=>{
+         const decoder = new TextDecoder()
+        for await (const chunk of proc.stdout) {
+            console.log(`>UI: ${decoder.decode(chunk)}`)
+        }
+    })
+   
+    
     return proc
 }
